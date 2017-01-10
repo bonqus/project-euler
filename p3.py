280b64394af6bcffb4eabf264b7e98a37cec6d86
Mathias Bohn Rasmussen <mathias_b2@hotmail.com>
2017-01-10 23:35:20 +0100

    p3

---
+++ p3.py
@@ -0,0 +1,19 @@
+def isPrime(num):
+    if (num < 2): return False
+    if (num == 2): return True
+    if (num % 2 == 0): return False
+    i = 3
+    while (i * i <= num):
+        if (num % i == 0): return False
+        i+=2
+    return True
+
+
+number = 600851475143
+x = int(number**0.5)
+i = 3
+
+while (i < x):
+    if (number%i == 0 and isPrime(i)):
+        print (i)
+    i+=2
